1271566a4bf966b82a1e71dbdcbcb883d1805905
adpusel <adrien.pusel@gmail.com>
2020-04-27 17:24:31 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/libftasm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libftasm.dir/main.c.o"
+  "libftasm"
+  "libftasm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libftasm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/libftasm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for libftasm.
+# This may be replaced when dependencies are built.
