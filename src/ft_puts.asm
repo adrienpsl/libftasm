a45ad4e3c332e6c0ae6fd222be857335fde47890
adpusel <adrien.pusel@gmail.com>
2020-05-04 18:41:59 +0200

    reset previous puts that's wasn't bug

--- src/ft_puts.asm
+++ src/ft_puts.asm
@@ -1,63 +1,62 @@
 %define MACH_SYSCALL(nb) 0x02000000 | nb
 %define STDOUT 1
 %define WRITE 4
-%define i r8
-%define ptr rdi
 
-%define size_arg
+%macro start 0
+    push rbp
+    mov  rbp,   rsp
+%endmacro
 
 %macro call_write 1
+    lea rsi, %1
+    mov rdi, STDOUT
+    mov rdx, r8
     mov rax, MACH_SYSCALL(WRITE)
-    mov rdi, STDOUT                          ; fd
-    lea rsi, %1                              ; str
-    mov rdx, i                               ; str size
     syscall
 
-    cmp rax, -1                              ; read fails
+    cmp rax, -1                     ; if function fuckup
     je handle_write_error
 
-    mov rax, 10                              ; return > 0 if no fail
+    mov rax, 10                     ; set ok return
 %endmacro
 
 section .text
     global _ft_puts
 
 _ft_puts:
-    push rbp
-    mov  rbp, rsp
+    start
+    cmp rdi, 0
+    je input_null
 
-    cmp rdi, 0                               ; input == NULL
-    je  input_null
 
-    mov i, 0                                 ; letter counter
-
+    mov r8, 0
 get_size:
-    cmp byte     [ptr + i], 0
-    je  write
-    inc i
+    cmp byte [rdi + r8], 0
+    je write
+    inc r8
     jmp get_size
 
 write:
-    call_write [ptr]
+    call_write [rdi]
+    mov r8, 1
     call_write [rel space]
     jmp out
 
-
-input_null:
-    mov        i,  null_str.len
-    call_write [rel null_str.string]
-    mov        rax, 10
-
 handle_write_error:
     mov rax, -1
+
+input_null:
+    mov r8, null.len
+    call_write[rel null.string]
+    mov rax, 10
 
 out:
     leave
     ret
 
 section .data
-    space: db 10
+space: db 10
 
-null_str:
+null:
     .string db "(null)", 10
-    .len equ $ - null_str.string
+    .len equ $ - null.string
